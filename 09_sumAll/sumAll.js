10e9e4557504d95e5da574059d51ea828f9860ff
Meris <mxg2@cornell.edu>
2025-07-21 18:22:04 -0400

    Complete exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    console.log(typeof start);
+    // Number.isInteger() is a useful function
+    if (typeof start != "number" || typeof end != "number" 
+        || start < 0 || end < 0 || start > Math.floor(start)
+        || end > Math.floor(end)
+    ) {
+        return "ERROR";
+    }
+    else if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    let result = 0;
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
