6a358752eb563e3bf92f235a388941efc4b0c87e
Meris <mxg2@cornell.edu>
2025-07-21 18:14:30 -0400

    Complete exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.from(arguments).slice(1);
+    for (arg of args) {
+        while(arr.includes(arg)) {
+            let index = arr.findIndex(argument => argument === arg);
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
