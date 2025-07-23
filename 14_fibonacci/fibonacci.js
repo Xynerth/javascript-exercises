3e11e6ae55500cecc3d10bba1b9c537b5c4eb3b6
Meris <mxg2@cornell.edu>
2025-07-23 14:51:59 -0400

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
+    if (num === 0) {
+        return 0;
+    }
+    else if (num < 0) {
+        return "OOPS";
+    }
 
+    let prev = 0;
+    let curr = 1;
+    let temp;
+
+    for (let i = 1; i < num; i++) {
+        temp = curr;
+        curr = curr + prev;
+        prev = temp;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
