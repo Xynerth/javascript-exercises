4acb926fba228f710f66ebd14d9fb1f20a31266b
Meris <mxg2@cornell.edu>
2025-07-23 14:43:56 -0400

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+function isLetter(char) {
+    return (char.toLowerCase() !== char.toUpperCase()) || parseInt(char);
+}
 
+function cleanString(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (!isLetter(str[i])) {
+            str = str.substring(0, i) + str.substring(i + 1);
+            i--;
+        }
+    }
+    return str;
+}
+
+const palindromes = function (str) {
+    str = cleanString(str);
+    str = str.toLowerCase();
+    let front = 0;
+    let back = str.length - 1;
+    while (front < back) {
+        if (str[front] !== str[back]) {
+            return false;
+        }
+        front++;
+        back--;
+    }
+    return true;
 };
 
 // Do not edit below this line
